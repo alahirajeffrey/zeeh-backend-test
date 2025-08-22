5463263bb2c19af6ce56be5ae67df3353f557239
alahirajeffrey <alahirajeffrey@gmail.com>
2025-08-22 02:29:32 -0700

    fix: removed parseInt function

--- src/common/config/config.config.ts
+++ src/common/config/config.config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const config: Config = {
   NODE_ENV: process.env.NODE_ENV || 'development',
-  PORT: parseInt(process.env.PORT) || 3000,
+  PORT: Number(process.env.PORT) || 3000,
   DATABASE_URL: process.env.DATABASE_URL || '',
   JWT_SECRET: process.env.JWT_SECRET || '',
 };
