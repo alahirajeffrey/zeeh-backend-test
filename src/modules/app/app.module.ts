4e5ed1d70835cb92e3f172e2f12d572a4c466b61
alahirajeffrey <alahirajeffrey@gmail.com>
2025-08-22 03:40:39 -0700

    imported auth module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { AuthModule } from '../auth';
 
 @Module({
   imports: [
+    AuthModule,
+    // rate limiter i.e 10 requests every 60 seconds
     ThrottlerModule.forRoot([
       {
         ttl: 60,
