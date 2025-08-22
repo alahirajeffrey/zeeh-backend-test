a4e0f896dd02d940dd1852ddf6fa8d94cfdb2a3d
alahirajeffrey <alahirajeffrey@gmail.com>
2025-08-22 02:23:55 -0700

    setup config variables

---
+++ src/common/config/config.config.ts
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv';
+import { Config } from '../interfaces';
+
+dotenv.config();
+
+export const config: Config = {
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  PORT: parseInt(process.env.PORT) || 3000,
+  DATABASE_URL: process.env.DATABASE_URL || '',
+  JWT_SECRET: process.env.JWT_SECRET || '',
+};
