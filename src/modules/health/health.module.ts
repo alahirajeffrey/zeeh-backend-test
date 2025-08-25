cffbb21945119351fd92d86fbe05ab7c17197e95
alahirajeffrey <alahirajeffrey@gmail.com>
2025-08-24 17:59:46 -0700

    imported jwt

--- src/modules/health/health.module.ts
+++ src/modules/health/health.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { PrismaService } from 'src/prisma.service';
 import { TerminusModule } from '@nestjs/terminus';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TerminusModule],
+  imports: [TerminusModule, JwtModule.register({})],
   controllers: [HealthController],
   providers: [PrismaService],
 })
